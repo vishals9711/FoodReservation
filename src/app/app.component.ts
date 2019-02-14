2690994ee8da7ead9979f819784632e874d04c4d
vishals9711 <vishalsharma1907@gmail.com>
2019-02-14 12:01:29 +0530

    modal fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from '../app/components/modal/modal.component';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -25,7 +26,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public modalController: ModalController
   ) {
     this.initializeApp();
   }
@@ -35,5 +37,13 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      componentProps: { value: 123 },
+      backdropDismiss:true
+    });
+    return await modal.present();
   }
 }
