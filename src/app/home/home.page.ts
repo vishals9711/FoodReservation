886f8b6f33192b7b8b132b8e4a1beeddfb065380
anand1296 <anandkhandalekar108@gmail.com>
2019-03-20 11:04:03 +0530

    asdasd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,12 +4,8 @@ import { Router } from '@angular/router';
 import { ModalComponent } from '../components/modal/modal.component';
 import { APIBackendService } from '../service/apibackend.service';
 import { RestaurantinfoService } from '../service/restaurantinfo.service';
-<<<<<<< HEAD
 import { Storage } from '@ionic/storage';
 import { Events } from '@ionic/angular';
-import { Router } from '@angular/router';
-=======
->>>>>>> 709b37c9058395d326ff7237b4e42325491f3102
 
 @Component({
   selector: 'app-home',
@@ -20,7 +16,6 @@ export class HomePage implements OnInit{
 
   public restaurantName: string = '';
   public restaurantAddress: string = '';
-<<<<<<< HEAD
   public allRestaurantData: any;
   
   constructor(public modalController: ModalController, public api: APIBackendService,
@@ -44,17 +39,6 @@ export class HomePage implements OnInit{
           // this.storage.get('restaurantAddress').then((RAddress) => {
           //   this.restaurantAddress = RAddress;
           // });
-=======
-  public allRestaurantData:any;
-
-  
-
-  constructor(public modalController: ModalController, public api: APIBackendService,
-     private router: Router, public restaurantAPI: RestaurantinfoService) {
-
-        this.restaurantAPI.getAllRestaurants().subscribe((data: {}) => {
-          this.allRestaurantData = data;
->>>>>>> 709b37c9058395d326ff7237b4e42325491f3102
     
         });
 
